83b8316eaef25d3165c5a0a0b52225fef6ebf955
Rajeev Kumar <46224203+rkcoola792@users.noreply.github.com>
2024-03-22 09:02:06 +0530

    router setup and basic folder setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <>
+     
+
+        <Header></Header>
+        <Outlet></Outlet>
+        <Footer></Footer>
+      </>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout></Layout>,
+      children: [
+        { path: "/", element: <Homepage></Homepage> },
+        // { path: "/marketing", element: <Marketing></Marketing> },
+      ],
+    },
+  ]);
+
   return (
-    <div className='App text-3xl'>
-     App
+    <div className="App ">
+      <RouterProvider router={router}></RouterProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
